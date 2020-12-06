e1b09b61674119aede52f47b271959423dc929e5
Rafael Amaral <rafael.duarte.amaral@gmail.com>
2020-12-06 11:11:25 -0300

    add mongodock jest

--- dist/database/config/mongo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    host: process.env.MONGO_URL || 'localhost',
-    port: process.env.MONGO_PORT || 27017,
-    username: process.env.MONGO_USER,
-    password: process.env.MONGO_PASS,
-    database: process.env.MONGO_DB,
-};
